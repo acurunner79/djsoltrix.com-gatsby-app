730bf3d38a80a5d63b5f71a034d23ee2b26b8710
Jorge <jorge@pop-os.localdomain>
2021-10-02 14:13:02 -0500

    formspree is set and working

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,18 +1,34 @@
 import React from 'react'
+import { Link } from 'gatsby'
 import Layout from "../components/Layout/index"
 import TopNav from '../components/Nav/TopNav'
 import Hamburger from '../components/Nav/Hamburger'
+import { ValidationError, useForm } from '@formspree/react'
 import '../styles/contact.css'
 
 const Contact = () => {
+
+    const [state, handleSubmit] = useForm("mpzknrwl");
+      if (state.succeeded) {
+        return (
+            <div className="message-submit">
+               <h1>Thank you!</h1>
+               <Link to="/">
+                  <button className="button"><h3>Back to home page</h3></button>   
+               </Link>
+            </div>
+         )
+      }
+
     return (
-        <Layout>
+        <>
         <TopNav />
         <Hamburger />
         <img id="upper-logo" src="https://res.cloudinary.com/acurunner79/image/upload/v1632361562/DJSoltrixWhite_ojfett.png"/>
         <div id="contact-main-container">
             <h1>Contact Page</h1>
-            <form>
+            <form onSubmit={handleSubmit} method="POST">
+            <ValidationError field="email" prefix="Email" errors={state.errors} />
                 <label className ="form-title">Name<br/>
                     <input className="form-input"
                         type="name"
@@ -45,10 +61,10 @@ const Contact = () => {
                     </textarea>
                 </label>
                     <br/>
-                <button className="button">Submit</button><br/>
+                <button className="button" type="submit" disabled={state.submitting}>Submit</button><br/>
             </form>
         </div>
-        </Layout>
+        </>
     )
 }
 
