76fb2782dea8fbe1b7e208fcd784027dd9799e00
Jorge <jorge@pop-os.localdomain>
2021-10-11 16:57:24 -0500

    started working on footer for mobile

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,25 @@
 import React from 'react'
 import { FaYoutube, FaInstagram, FaFacebook, FaMixcloud } from 'react-icons/fa'
+import { HiOutlineMail} from 'react-icons/hi'
+import '../../styles/footer.css'
 
 const Footer = () => {
     return (
-        <div id="social-media-icons">
-            <p>Djsoltrix.com</p>
-            <FaYoutube size="30" color="white"/>
-            <FaInstagram size="30" color="white"/>
-            <FaFacebook size="30" color="white"/>
-            <FaMixcloud size="30" color="white"/>
+        <>
+        <div id="footer-container">
+            <div>
+                <p><img id="footer-logo" src="https://res.cloudinary.com/acurunner79/image/upload/v1633740784/cropped-soltrix-logo_uu5wpm.png" alt=""/></p>
+            </div>
+            <div id="social-media-icons">
+                <a target="_blank"><FaYoutube className="footer-icon" size="30" color="white"/></a>
+                <FaInstagram className="footer-icon" size="30" color="white"/>
+                <FaFacebook className="footer-icon" size="30" color="white"/>
+                <FaMixcloud className="footer-icon" size="30" color="white"/>
+            </div>
         </div>
+                <p><HiOutlineMail />bookings@djsoltrix.com</p>
+        <p>&copy;2021</p>
+        </>
     )
 }
 
