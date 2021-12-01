70d5d0e3b3a1b9f462b7a0257455dfd8edd0ccdc
acurunner79 <djsmoothzouk@gmail.com>
2021-11-30 23:37:45 -0600

    trigger build for contentful

--- src/components/RichText/index.js
+++ src/components/RichText/index.js
@@ -19,11 +19,11 @@ const RichText = ({ assetRef, raw }) => {
                 const asset = assetRef.fixed;
                 // console.log('asset', assetRef.fixed.src)
 
-              if ((assetRef.fixed.src).includes('//images')){
+            //   if ((assetRef.fixed.src).includes('//images')){
               return (
                 <img id="asset-image" src={asset.src} alt={asset.title} />
               );
-              }
+            //   }
             },
             [INLINES.HYPERLINK]: (node) => {
                 // console.log(node)
